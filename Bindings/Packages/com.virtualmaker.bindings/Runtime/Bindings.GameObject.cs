3ac6f4c66e868bf0cd133965cd2a32242c155a9e
NicholasPerell <nicholasperell@gmail.com>
2024-11-27 16:08:37 -0500

    BindList added

--- Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.GameObject.cs
+++ Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.GameObject.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace VirtualMaker.Bindings
@@ -24,5 +25,60 @@ namespace VirtualMaker.Bindings
         {
             Bind(prop, value => component.gameObject.SetActive(transformer(value)));
         }
+
+        public void BindList<FromType, ToType>(Transform parent, ToType prefab, IProperty<List<FromType>> prop, Action<FromType, ToType> onTemplateAdded) where ToType : Component
+        {
+            var childItems = new Dictionary<FromType, ToType>();
+
+            Action<List<FromType>> update = list =>
+            {
+                // Remove items that are no longer in the list.
+                var removed = new List<FromType>();
+
+                foreach (var (fromItem, toItem) in childItems)
+                {
+                    // In edit mode remove everything so we can test data changes
+                    if (!list.Contains(fromItem) || !Application.isPlaying)
+                    {
+                        GameObject.Destroy(toItem.gameObject);
+                        removed.Add(fromItem);
+                    }
+                }
+
+                foreach (var item in removed)
+                {
+                    childItems.Remove(item);
+                }
+
+                // Add items that are new to the list.
+                for (int i = 0; i < list.Count; i++)
+                {
+                    var item = list[i];
+                    if (!childItems.TryGetValue(item, out var toItem))
+                    {
+                        toItem = GameObject.Instantiate(prefab, parent);
+                        childItems.Add(item, toItem);
+                        onTemplateAdded(item, toItem);
+                    }
+
+                    toItem.transform.SetSiblingIndex(i);
+                }
+            };
+
+            prop.OnChange += update;
+
+            _unsubscribe.Add(() =>
+            {
+                prop.OnChange -= update;
+
+                foreach (var (_, toItem) in childItems)
+                {
+                    GameObject.Destroy(toItem.gameObject);
+                }
+            });
+
+            update(prop.Value);
+        }
+
     }
 }
