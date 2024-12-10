38e46b8addd3203d0ab2336704dc6f424fe0e3f8
NicholasPerell <nicholasperell@gmail.com>
2024-12-10 11:05:19 -0500

    Added summary comments to other methods

--- Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.PlayerPrefs.cs
+++ Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.PlayerPrefs.cs
@@ -18,12 +18,20 @@ namespace VirtualMaker.Bindings
             BindPlayerPref(key, prop);
         }
 
+        /// <summary> Binds player pref to property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to set the value. </param>
+        /// <param name="prop"> <c>IProperty</c> that sets the value. </param>
         public void BindPlayerPref(string key, IProperty<float> prop)
         {
             Bind(prop, value => PlayerPrefs.SetFloat(key, value));
             SavePrefsOnReset();
         }
 
+        /// <summary> Binds player pref to property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
+        /// <param name="prop"> Property that sets the value. </param>
+        /// <param name="getFromPref"> Whether to try setting <c>prop.Value</c> to the value already inside the <c>PlayerPrefs</c>. </param>
+        /// <param name="defaultValue"> Value to set <c>prop.Value</c> to if <c>getFromPref</c> is <c>true</c> but there isn't a value already inside the <c>PlayerPref</c>. </param>
         public void BindPlayerPref(string key, Property<int> prop, bool getFromPref, int? defaultValue = null)
         {
             if (getFromPref)
@@ -33,12 +41,20 @@ namespace VirtualMaker.Bindings
             BindPlayerPref(key, prop);
         }
 
+        /// <summary> Binds player pref to property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to set the value. </param>
+        /// <param name="prop"> <c>IProperty</c> that sets the value. </param>
         public void BindPlayerPref(string key, IProperty<int> prop)
         {
             Bind(prop, value => PlayerPrefs.SetInt(key, value));
             SavePrefsOnReset();
         }
 
+        /// <summary> Binds player pref to property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
+        /// <param name="prop"> Property that sets the value. </param>
+        /// <param name="getFromPref"> Whether to try setting <c>prop.Value</c> to the value already inside the <c>PlayerPrefs</c>. </param>
+        /// <param name="defaultValue"> Value to set <c>prop.Value</c> to if <c>getFromPref</c> is <c>true</c> but there isn't a value already inside the <c>PlayerPref</c>. </param>
         public void BindPlayerPref(string key, Property<string> prop, bool getFromPref, string defaultValue)
         {
             if (getFromPref)
@@ -48,6 +64,10 @@ namespace VirtualMaker.Bindings
             BindPlayerPref(key, prop);
         }
 
+        /// <summary> Binds player pref to property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
+        /// <param name="prop"> Property that sets the value. </param>
+        /// <param name="getFromPref"> Whether to try setting <c>prop.Value</c> to the value already inside the <c>PlayerPrefs</c>. </param>
         public void BindPlayerPref(string key, Property<string> prop, bool getFromPref)
         {
             if (getFromPref)
@@ -57,6 +77,9 @@ namespace VirtualMaker.Bindings
             BindPlayerPref(key, prop);
         }
 
+        /// <summary> Binds player pref to property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to set the value. </param>
+        /// <param name="prop"> <c>IProperty</c> that sets the value. </param>
         public void BindPlayerPref(string key, IProperty<string> prop)
         {
             Bind(prop, value => PlayerPrefs.SetString(key, value));
