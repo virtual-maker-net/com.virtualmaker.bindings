ec581dfc352eb1768630d338e2c3c8176e4e3965
NicholasPerell <nicholasperell@gmail.com>
2025-01-30 20:43:11 -0500

    Pair programming

--- Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.PlayerPrefs.cs
+++ Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.PlayerPrefs.cs
@@ -4,23 +4,14 @@ namespace VirtualMaker.Bindings
 {
     public partial class Bindings
     {
-        /// <summary> Binds player pref to property. </summary>
-        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
-        /// <param name="prop"> <c>Property</c> that gets the stored value then sets it when changed. </param>
+        /// <summary> Assigns property to saved player pref, then binds player pref to be set by the property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to set the value. </param>
+        /// <param name="prop"> <c>Property</c> that sets the value. </param>
         /// <param name="defaultValue"> Optional: Value to set <c>prop.Value</c> to if a value isn't found. </param>
-        public void BindPlayerPref(string key, Property<float> prop, float defaultValue)
+        public void GetAndBindPlayerPref(string key, Property<float> prop, float defaultValue = 0)
         {
             prop.Value = PlayerPrefs.GetFloat(key, defaultValue);
-            BindPlayerPref(key, (IProperty<float>)prop);
-        }
-
-        /// <summary> Binds player pref to property. </summary>
-        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
-        /// <param name="prop"> <c>Property</c> that gets the stored value then sets it when changed. </param>
-        public void BindPlayerPref(string key, Property<float> prop)
-        {
-            prop.Value = PlayerPrefs.GetFloat(key);
-            BindPlayerPref(key, (IProperty<float>)prop);
+            BindDeferred(prop, value => PlayerPrefs.SetFloat(key, value));
         }
 
         /// <summary> Binds player pref to be set by an <c>IProperty</c>. </summary>
@@ -31,23 +22,14 @@ namespace VirtualMaker.Bindings
             Bind(prop, value => PlayerPrefs.SetFloat(key, value));
         }
 
-        /// <summary> Binds player pref to property. </summary>
-        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
-        /// <param name="prop"> <c>Property</c> that gets the stored value then sets it when changed. </param>
+        /// <summary> Assigns property to saved player pref, then binds player pref to be set by the property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to set the value. </param>
+        /// <param name="prop"> <c>Property</c> that sets the value. </param>
         /// <param name="defaultValue"> Optional: Value to set <c>prop.Value</c> to if a value isn't found. </param>
-        public void BindPlayerPref(string key, Property<int> prop, int defaultValue)
+        public void GetAndBindPlayerPref(string key, Property<int> prop, int defaultValue = 0)
         {
             prop.Value = PlayerPrefs.GetInt(key, defaultValue);
-            BindPlayerPref(key, (IProperty<int>)prop);
-        }
-
-        /// <summary> Binds player pref to property. </summary>
-        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
-        /// <param name="prop"> <c>Property</c> that gets the stored value then sets it when changed. </param>
-        public void BindPlayerPref(string key, Property<int> prop)
-        {
-            prop.Value = PlayerPrefs.GetInt(key);
-            BindPlayerPref(key, (IProperty<int>)prop);
+            BindDeferred(prop, value => PlayerPrefs.SetInt(key, value));
         }
 
         /// <summary> Binds player pref to be set by an <c>IProperty</c>. </summary>
@@ -58,24 +40,14 @@ namespace VirtualMaker.Bindings
             Bind(prop, value => PlayerPrefs.SetInt(key, value));
         }
 
-        /// <summary> Binds player pref to property. </summary>
-        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
-        /// <param name="prop"> <c>Property</c> that gets the stored value then sets it when changed. </param>
-        /// <param name="defaultValue"> Value to set <c>prop.Value</c> to if a value isn't found. </param>
-        public void BindPlayerPref(string key, Property<string> prop, string defaultValue)
+        /// <summary> Assigns property to saved player pref, then binds player pref to be set by the property. </summary>
+        /// <param name="key"> Key used by <c>PlayerPrefs</c> to set the value. </param>
+        /// <param name="prop"> <c>Property</c> that sets the value. </param>
+        /// <param name="defaultValue"> Optional: Value to set <c>prop.Value</c> to if a value isn't found. </param>
+        public void GetAndBindPlayerPref(string key, Property<string> prop, string defaultValue = "")
         {
             prop.Value = PlayerPrefs.GetString(key, defaultValue);
-            BindPlayerPref(key, (IProperty<string>)prop);
-        }
-
-        /// <summary> Binds player pref to property. </summary>
-        /// <param name="key"> Key used by <c>PlayerPrefs</c> to get and set the value. </param>
-        /// <param name="prop"> <c>Property</c> that gets the stored value then sets it when changed. </param>
-        /// <param name="defaultValue"> Optional: Value to set <c>prop.Value</c> to if a value isn't found. </param>
-        public void BindPlayerPref(string key, Property<string> prop)
-        {
-            prop.Value = PlayerPrefs.GetString(key);
-            BindPlayerPref(key, (IProperty<string>)prop);
+            BindDeferred(prop, value => PlayerPrefs.SetString(key, value));
         }
 
         /// <summary> Binds player pref to be set by an <c>IProperty</c>. </summary>
