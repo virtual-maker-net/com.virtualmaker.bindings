c50ea2b192cea4396901d6312e7e9f56b7f792fb
Alon Farchy <afarchy@virtualmaker.net>
2025-06-24 12:35:25 -0500

    ID version

--- Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.Material.cs
+++ Bindings/Packages/com.virtualmaker.bindings/Runtime/Bindings.Material.cs
@@ -15,5 +15,17 @@ namespace VirtualMaker.Bindings
         {
             Bind(prop, value => SetTexture(material, propertyName, value));
         }
+
+        private async void SetTexture(Material material, int propertyId, string url)
+        {
+            var texture = await ImageDownloader.DownloadImageAsync(url);
+            material.SetTexture(propertyId, texture);
+            _unsubscribe.Add(() => ImageDownloader.ReleaseImage(texture));
+        }
+
+        public void BindTexture(Material material, int propertyId, IProperty<string> prop)
+        {
+            Bind(prop, value => SetTexture(material, propertyId, value));
+        }
     }
 }
